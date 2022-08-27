02fa80389400f5e840e448834ae1cee2b3dc27fa
Prasad <prasadjs178@gmail.com>
2022-08-27 12:28:46 +0530

    jquery w/ typescript implemented with logic

--- scripts.ts
+++ scripts.ts
@@ -1,9 +1,10 @@
-var expr = "";
-var flag = 0;
-function input(char) {
-    if (expr[expr.length - 1] == '+' || expr[expr.length - 1] == '-' || expr[expr.length - 1] == '*' || expr[expr.length - 1] == '/') {
+var expr: string = ""
+var flag: number = 0;
+
+function input(char:string) {
+    if (expr[expr.length-1] == '+' || expr[expr.length-1] == '-' || expr[expr.length-1] == '*' || expr[expr.length-1] == '/') {
         if (char == '+' || char == '-' || char == '*' || char == '/') {
-            expr = expr.slice(0, -1);
+            expr = expr.slice(0,-1);
             expr += char;
             $(".output").val(expr);
             flag = 0;
@@ -22,25 +23,28 @@ function input(char) {
         $(".output").val(expr);
     }
 }
+
 function displayOutput() {
     $(".output").val(eval(expr));
     expr = eval(expr);
     flag = 1;
 }
+
 function resetValues() {
     expr = "";
     $(".output").val("");
 }
+
 function deleteLast() {
-    if (expr[expr.length - 1] == '+' || expr[expr.length - 1] == '-' || expr[expr.length - 1] == '*' || expr[expr.length - 1] == '/') {
-        expr = expr.slice(0, -1);
+    if (expr[expr.length-1] == '+' || expr[expr.length-1] == '-' || expr[expr.length-1] == '*' || expr[expr.length-1] == '/') {
+        expr = expr.slice(0,-1);
         $(".output").val(expr);
     }
     else if (flag == 1) {
         return;
     }
     else if (flag == 0) {
-        expr = expr.slice(0, -1);
+        expr = expr.slice(0,-1);
         $(".output").val(expr);
     }
 }
